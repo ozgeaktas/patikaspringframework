69b2d8cc3650649892d654ca9a008576a62f221c
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2023-01-18 01:43:55 +0300

    named

---
+++ src/main/java/com/ozgeaktas/patikaspring/cdi/_01_Named.java
@@ -0,0 +1,24 @@
+package com.ozgeaktas.patikaspring.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+
+//cdi ==> bean
+@Named(value = "namedtuto")
+@ApplicationScoped
+public class _01_Named {
+
+    private Long namedId;
+    private String namedData="Named Cdi bean";
+}
