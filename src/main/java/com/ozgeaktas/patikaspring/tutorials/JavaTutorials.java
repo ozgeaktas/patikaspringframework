7fc44a1c2dad187d46868a9adf9f34de824bc9b5
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2023-01-17 23:37:57 +0300

    hatırlatma

---
+++ src/main/java/com/ozgeaktas/patikaspring/tutorials/JavaTutorials.java
@@ -0,0 +1,9 @@
+package com.ozgeaktas.patikaspring.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+       // Arrays.sort();
+    }
+}
